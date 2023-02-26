7da54c387d93e7b024f5c87348efa057acb3bc9e
Lubpui <98448740+Lubpui@users.noreply.github.com>
2023-02-27 02:47:54 +0700

    week2(v2)

--- client/src/App.js
+++ client/src/App.js
@@ -1,96 +1,23 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ImagePredict from "./components/ImagePredict";
+import "./css/App.css";
+import ImageConverter from "./components/ImageConverter";
 
-function App() {
-  const [image, setImage] = useState(null);
-  const [converted, setConverted] = useState(null);
-  const [error, setError] = useState(null);
-  const [format, setFormat] = useState("png");
-
-  const handleImageChange = (e) => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
-      setConverted(null);
-      setError(null);
-    }
-  };
-
-  const handleFormatChange = (e) => {
-    setFormat(e.target.value);
-  };
-
-  const handleImageUpload = async () => {
-    if (!image) {
-      setError("Please select an image to convert.");
-      return;
-    }
-    const allowedFormats = ["png", "jpeg", "gif"];
-    if (!allowedFormats.includes(format)) {
-      setError(`Unsupported output format: ${format}.`);
-      return;
-    }
-    const formData = new FormData();
-    formData.append("image", image);
-    formData.append("format", format);
-    try {
-      const response = await fetch(
-        "https://lubpui-psychic-eureka-xg9qj95wgjp3p6vg-5000.preview.app.github.dev/convert",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Conversion failed.");
-      }
-      const data = await response.blob();
-      setConverted(URL.createObjectURL(data));
-      setError(null);
-    } catch (err) {
-      setError("Conversion failed.");
-    }
-  };
-
+const App = () => {
   return (
-    <div className="App">
-      <div className="BGIMG" />
-      <div className="content">
-        <h1>Image Converter</h1>
-
-        <div className="format-box">
-          <label htmlFor="format">Output Format : </label>
-          <select id="format" value={format} onChange={handleFormatChange}>
-            <option value="png">PNG</option>
-            <option value="jpeg">JPEG</option>
-            <option value="gif">GIF</option>
-          </select>
-        </div>
-
-        <div className="img-box">
-          {image && <img src={URL.createObjectURL(image)} alt="Original" />}
-          {converted && <img src={converted} alt="Converted" />}
-        </div>
-        {error && <p>{error}</p>}
-        <div className="button-box">
-          <input
-            className="btn2"
-            id="upload-button"
-            type="file"
-            onChange={handleImageChange}
-          />
-          <label className="btn2" for="upload-button">
-            <FontAwesomeIcon className="fontAS" icon={faArrowUpFromBracket} />
-            upload
-          </label>
-          <button className="btn" onClick={handleImageUpload}>
-            Convert
-          </button>
-        </div>
-      </div>
+    <div className="BGIMG">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<ImagePredict />} />
+          <Route exact path="/ImageConverter" element={<ImageConverter />} />
+          <Route exact path="/ImagePredict" element={<ImagePredict />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
